d60fe67c199e8c25d6fedae57d7124ea9ebb62b0
WangMinan <wangminan0811@hotmail.com>
2021-12-13 14:16:15 +0800

    Finish the structure of MainEntrance

--- LuckyHostProject/src/com/game/MainEntrance.java
+++ LuckyHostProject/src/com/game/MainEntrance.java
@@ -1,9 +1,28 @@
 package com.game;
 
+import javax.swing.*;
+
 /**
  * @author WangMinan
- * @description
+ * @description To define the main entrance of the game
  * @see
  */
 public class MainEntrance {
+    private static JFrame mainFrame= new JFrame("Lucky Host");
+
+    public void playNewGame(){
+
+    }
+
+    public void playSavedGame(){
+
+    }
+
+    public void exit(){
+
+    }
+
+    public static void main(String args[]){
+
+    }
 }
