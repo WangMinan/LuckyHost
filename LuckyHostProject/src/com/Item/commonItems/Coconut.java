0a59f62e7a33502a1bcd3cf1a9ace8d0ee472aba
WangMinan <wangminan0811@hotmail.com>
2021-12-14 21:20:27 +0800

    Finish Coconut

---
+++ LuckyHostProject/src/com/Item/commonItems/Coconut.java
@@ -0,0 +1,25 @@
+package com.Item.commonItems;
+
+import com.Item.CommonItem;
+import com.Item.ItemCategory;
+
+import javax.swing.*;
+
+/**
+ * @author
+ * @description
+ * @see
+ */
+public class Coconut extends CommonItem {
+
+    public Coconut(){
+        this.setName("coconut");
+        this.setIcon(new JButton(new ImageIcon("images/coconut")));
+        this.setDescription("价值4金币");
+    }
+
+    @Override
+    public int calculateMoney(ItemCategory category) {
+        return 4;
+    }
+}
