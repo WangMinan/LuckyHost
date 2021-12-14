650b141800d168eff9b3147a49d7be308a6b5d41
WangMinan <wangminan0811@hotmail.com>
2021-12-14 20:33:28 +0800

    图片的修正

---
+++ LuckyHostProject/src/com/Item/specialItems/RainCloud.java
@@ -0,0 +1,30 @@
+package com.Item.specialItems;
+
+import com.Item.ItemCategory;
+import com.Item.SpecialItem;
+import javax.swing.*;
+
+/**
+ * @author Wangminan
+ * @description 雨云类
+ * @see SpecialItem
+ */
+public class RainCloud extends SpecialItem {
+
+    public RainCloud() {
+        this.setName("rainCloud");
+        this.setIcon(new JButton(new ImageIcon("images/specialItems/rainCloud.png")));
+        this.setDescription("雨每次+1金币");
+    }
+
+    @Override
+    public int calculateMoney(ItemCategory category){
+        int cnt = 0;
+        for(int i=0; i<20; i++){
+            if(category.getItemCategory().elementAt(i).getName().equals("rain")){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
