f1dca5cd4f2eb748af0f9c4c400dfe649fd13cda
tanknok <1633434682@qq.com>
2021-12-14 20:39:19 +0800

    提交Empty，修改LockRmover

---
+++ LuckyHostProject/src/com/Item/commonItems/Empty.java
@@ -0,0 +1,20 @@
+package com.Item.commonItems;
+
+import com.Item.CommonItem;
+import com.Item.ItemCategory;
+
+import javax.swing.*;
+
+public class Empty extends CommonItem {
+    public Empty(){
+        this.setPriority(3);
+        this.setName("Empty");
+        this.setIcon(new JButton(new ImageIcon("images/empty.png")));
+        this.setDescription("");
+    }
+
+    public int calculateMoney(ItemCategory category){
+        int price =0;
+        return price;
+    }
+}
