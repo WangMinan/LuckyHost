ca45db3e7fe4fe356aa69edd2a166444067969d3
tanknok <1633434682@qq.com>
2021-12-14 21:18:19 +0800

    修改bubble

--- LuckyHostProject/src/com/Item/commonItems/Bubble.java
+++ LuckyHostProject/src/com/Item/commonItems/Bubble.java
@@ -17,7 +17,7 @@ public class Bubble extends CommonItem {
 
     public Bubble(){
         this.setPriority(1);
-        this.setName("bulle");
+        this.setName("bubble");
         this.setIcon(new JButton(new ImageIcon("images/bulle.png")));
         this.setDescription("价值2枚金币");
     }
