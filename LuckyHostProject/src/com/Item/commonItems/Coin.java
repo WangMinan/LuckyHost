8136bca3bf852505db7d5b9b9cc8e1cafc603817
tanknok <1633434682@qq.com>
2021-12-14 21:24:30 +0800

    提交Coin

---
+++ LuckyHostProject/src/com/Item/commonItems/Coin.java
@@ -0,0 +1,19 @@
+package com.Item.commonItems;
+
+import com.Item.CommonItem;
+import com.Item.ItemCategory;
+
+import javax.swing.*;
+
+public class Coin extends CommonItem {
+    public Coin(){
+        this.setPriority(3);
+        this.setName("coin");
+        this.setIcon(new JButton(new ImageIcon("images/coin.png")));
+        this.setDescription("价值1枚金币");
+    }
+
+    public int calculateMoney(ItemCategory category){
+        return 1;
+    }
+}
