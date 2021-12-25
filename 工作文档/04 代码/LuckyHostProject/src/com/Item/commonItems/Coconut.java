e2b1e4d8444d9d68d06acbb0bf44b2088b7688a7
WangMinan <wangminan0811@hotmail.com>
2021-12-25 14:37:55 +0800

    增加了提交文件

---
+++ 工作文档/04 代码/LuckyHostProject/src/com/Item/commonItems/Coconut.java
@@ -0,0 +1,32 @@
+package com.Item.commonItems;
+
+import com.Item.CommonItem;
+import com.Item.ItemCategory;
+
+import javax.swing.*;
+
+/**
+ * @author Wangminan
+ * @description 椰子类
+ * @see CommonItem
+ */
+public class Coconut extends CommonItem {
+
+    public Coconut(){
+        this.setPriority(1);
+        this.setName("coconut");
+        this.setIcon(new JButton(new ImageIcon("images/commonItems/coconut.png")));
+        this.getIcon().setFocusPainted(false);
+        this.setDescription("价值2金币,被消除后给予10金币");
+    }
+
+    @Override
+    public int calculateMoney(ItemCategory category) {
+        return 2;
+    }
+
+    @Override
+    public Coconut createNewItem(){
+        return new Coconut();
+    }
+}
---
+++ 工作文档/04 代码/LuckyHostProject/src/com/Item/commonItems/Cow.java
@@ -0,0 +1,56 @@
+package com.Item.commonItems;
+
+import com.Item.CommonItem;
+import com.Item.ItemCategory;
+
+import javax.swing.*;
+import java.util.Random;
+
+/**
+ * @author Wangminan
+ * @description 奶牛类
+ * @see CommonItem
+ */
+public class Cow extends CommonItem {
+
+    public Cow(){
+        this.setName("cow");
+        this.setIcon(new JButton(new ImageIcon("images/commonItems/cow.png")));
+        this.getIcon().setFocusPainted(false);
+        this.setDescription("价值3金币,如果老虎机面板上有空格,有15%概率增加牛奶");
+        this.setPriority(4);
+    }
+
+    @Override
+    public int calculateMoney(ItemCategory category) {
+        //计算空格
+        int cntEmpty = -1;
+        int[] pos = new int[20];
+        for(int i = 0; i<20; i++){
+            if (category.getItemCategory().elementAt(i).getName().equals("empty")){
+                cntEmpty ++;
+                pos[cntEmpty] = i;
+            }
+        }
+        if(cntEmpty!=-1){
+            //概率判定
+            Random rand =new Random();
+            int random = rand.nextInt(100);
+            if(random < 15){
+                if(cntEmpty == 0){
+                    category.getItemCategory().setElementAt(new Milk(),pos[0]);
+                } else {
+                    int rnd = rand.nextInt(cntEmpty+1);
+                    category.getItemCategory().setElementAt(new Milk(),pos[rnd]);
+                }
+            }
+
+        }
+        return 3;
+    }
+
+    @Override
+    public Cow createNewItem(){
+        return new Cow();
+    }
+}
